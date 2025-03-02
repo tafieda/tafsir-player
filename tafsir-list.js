98d84e7fb59669de8e7db30f620a485b691ef425
Khalil Jamil Ahmad <khaliljamil12@aol.com>
2025-03-02 05:27:08 +0100

    Update tafsir-list.js

--- tafsir-list.js
+++ tafsir-list.js
@@ -1,10 +1,10 @@
 let allMusic = [
-   /*{
+    {
         name: "001 Suratu Ali Imran",
         artist: "Prof Isa Ali Ibrahim Pantami",
         img: "music-1",
         src: "Suratu%20Ali%20Imran"
-    },*/
+    },
     {
         name: "001 Suratul Hujrat",
         artist: "Sheikh Ahmad Tijjani Guruntum",
